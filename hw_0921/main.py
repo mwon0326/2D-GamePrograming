3ae42aea5278295dcd98f9946a608dd44165d2b1
mwon0326 <mwon0326@gmail.com>
2018-09-27 13:27:47 +0900

    마우스 클릭에 따라 클릭한 순서대로 따라가기

--- hw_0921/main.py
+++ hw_0921/main.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import random
+import math
 
 class Grass:
     def __init__(self):
@@ -9,30 +10,29 @@ class Grass:
         self.image.draw(400,30)
 
 class Boy:
-    global x, y
+    global waypoints
     def __init__(self):
         self.x, self.y = random.randint(0, 200), random.randint(90, 550)
         self.frame = random.randint(0, 7)
         self.image = load_image('../image/run_animation.png')
         self.speed = random.uniform(1.0, 3.0)
-
+        self.i = 0
+        
     def update(self):
-        if x > self.x:
-            self.x += self.speed
-            if x <= self.x:
-                self.x = x
-        elif x < self.x:
-            self.x -= self.speed
-            if x >= self.x:
-                self.x = x
-        if y > self.y:
-            self.y += self.speed
-            if y <= self.y:
-                self.y = y
-        elif y < self.y:
-            self.y -= self.speed
-            if y >= self.y:
-                self.y = y
+        if len(waypoints) > 0:
+            (x, y) = waypoints[self.i]
+            dx, dy = x - self.x, y - self.y
+            dist = math.sqrt(dx ** 2 + dy ** 2)
+            if dist > 0:
+                self.x += self.speed * dx / dist
+                self.y += self.speed * dy / dist
+
+                if dx < 0 and self.x < x: self.x = x
+                if dx > 0 and self.x > x: self.x = x
+                if dy < 0 and self.y < y: self.y = y
+                if dy > 0 and self.y > y: self.y = y
+            if self.x == x and self.y == y and self.i < len(waypoints) - 1:
+                self.i += 1
         self.frame = (self.frame + 1) % 8
 
     def draw(self):
@@ -40,7 +40,7 @@ class Boy:
 
 def hanble_events():
     global running
-    global x, y
+    global waypoints
     
     events = get_events()
 
@@ -50,16 +50,18 @@ def hanble_events():
         elif e.type == SDL_KEYDOWN:
             if e.key == SDLK_ESCAPE:
                 running = False
-        elif e.type == SDL_MOUSEMOTION:
-                x = e.x
-                y = 600 - e.y
+        elif e.type == SDL_MOUSEBUTTONDOWN:
+            x, y = e.x, 600 - e.y
+            waypoints += [(x, y)]
 
 open_canvas()
 g = Grass()
 boys = []
 for i in range(20):
     boys += [Boy()]
-x,y = 0, 0
+    
+waypoints = []
+wp = load_image('../image/wp.png')
 #boys = [Boys() for i in range(20)]
     
 running = True
@@ -68,6 +70,10 @@ while running:
         
     clear_canvas()
     g.draw()
+    
+    for loc in waypoints:
+        wp.draw(loc[0], loc[1])
+        
     for b in boys:
         b.draw()
 
