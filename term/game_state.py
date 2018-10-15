92abf3ae8ad48ce40cf8dc2693772e5297cb759d
mwon0326 <mwon0326@gmail.com>
2018-10-15 13:17:20 +0900

    캐릭터 좌우 이동 캐릭터가 화면 중간까지 갔을 때 배경 이동

--- term/game_state.py
+++ term/game_state.py
@@ -2,9 +2,11 @@ from pico2d import *
 import  game_framework
 
 class Castle:
+    global backgroundX
     image = None
+
     def __init__(self):
-        self.x = 800
+        Castle.backgroundX = 800
         self.stage1 = load_image('resource/노을.png')
         self.stage2 = load_image('resource/저녁.png')
         self.stage3 = load_image('resource/밤.png')
@@ -14,7 +16,7 @@ class Castle:
         Castle.image = self.stage1
 
     def draw(self):
-        self.image.clip_draw(self.x, 0, 800, 600, 400, 300)
+        self.image.clip_draw(Castle.backgroundX, 0, 800, 600, 400, 300)
 
     def change(self):
         self.count += 1
@@ -28,21 +30,35 @@ class Castle:
             Castle.image = self.stage5
 
 class Princess:
-    global rightP, leftP
-    global frame, state
-    global x, y
     image = None
-
+    global state, backgroundX
     def __init__(self):
         self.rightP = load_image('resource/princess_right.png')
         self.leftP = load_image('resource/princess_left.png')
         self.frame = 0
-        self.state = 0
+        Princess.state = 0
         self.x, self.y = 700, 200
         Princess.image = self.leftP
 
     def draw(self):
-       self.image.clip_draw(self.frame * 100, 0, 200, 300, self.x, self.y)
+       self.image.clip_draw(self.frame * 200, 0, 200, 300, self.x, self.y)
+
+    def update(self):
+        if state == 0:
+            Princess.image = self.leftP
+            self.x -= 50
+        elif state == 1:
+            Princess.image = self.rightP
+            self.x += 50
+
+        if self.x <= 400 and Castle.backgroundX >= 400:
+            self.x = 400
+            if state == 0:
+                Castle.backgroundX -= 50
+            elif state == 1:
+                Castle.backgroundX += 50
+
+        self.frame = (self.frame + 1) % 4
 
 def enter():
     global  c, p
@@ -70,8 +86,18 @@ def resume():
     pass
 
 def handle_events():
-    pass
+    global p
+    global state
+    events = get_events()
 
+    for e in events:
+        if e.type == SDL_KEYDOWN:
+            if e.key == SDLK_LEFT:
+                state = 0
+                p.update()
+            elif e.key == SDLK_RIGHT:
+                state = 1
+                p.update()
 
 if __name__ == '__main__':
     main()
